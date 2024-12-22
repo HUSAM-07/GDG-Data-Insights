6b06900dbaa7f111744c4b2ec5e802e637a71026
Mohammed Husamuddin <uni.husam@gmail.com>
2024-12-22 17:57:00 +0400

    new app

--- utils/email_utils.py
+++
@@ -1,4 +0,0 @@
-def get_email_list(df, column_name='Email IDs'):
-    """Extract and format email addresses from dataframe"""
-    emails = df[column_name].dropna().tolist()
-    return '; '.join(emails) 
